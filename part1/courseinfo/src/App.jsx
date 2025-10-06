aedecd942ab56b341977da0dbd55a219f72f7882
Felipe Garcia <luisfelipeglondo@gmail.com>
2025-10-05 22:55:16 -0500

    refactor solution for exercise 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={ { part1, part2, part3 } } exercises={ { exercises1, exercises2, exercises3 }} />
+      <Content parts={{ part1, part2, part3 }} exercises={{ exercises1, exercises2, exercises3 }} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
