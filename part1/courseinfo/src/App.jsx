7bb740a2172b2e683fa72e908b58a114b0a08d92
Felipe Garcia <luisfelipeglondo@gmail.com>
2025-10-06 22:28:38 -0500

    Correction solution for exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,7 +18,7 @@ const Part = (props) => (
 )
 
 const Total = (props) => (
-  <p>Number of exercises { props.total }</p>
+  <p>Number of exercises { props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
 )
 
 const App = () => {
@@ -42,7 +42,7 @@ const App = () => {
     <div>
       <Header course={ course } />
       <Content parts={ parts } />
-      <Total total={ parts[0].exercises + parts[1].exercises + parts[2].exercises } />
+      <Total parts={ parts } />
     </div>
   )
 }
